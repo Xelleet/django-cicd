72df7623ffb6cb27f9c43e5afefb0677a6437eee
Xelleet <ererenyeager@yandex.ru>
2026-05-23 15:36:00 +0300

    test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class CalculatorViewTests(TestCase):
+
+    def test_index_post_valid_data(self):
+        response = self.client.post(self.url, {
+            'num1': '5',
+            'num2': '3'
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertEqual(response.context['result'], 8)
